ec5de69c2b74ee7bc9f166a0f32156e408b63993
Yassin Youssoufm <yassinrsc@gmail.com>
2022-08-19 21:50:09 +0300

    Image uploading

--- pages/home.vue
+++ pages/home.vue
@@ -2,29 +2,29 @@
   <div class="">
     <div class="w-12/12 mx-auto px-8 mt-16">
       <div class="flex items-center justify-end">
-        <button class="btn btn-primary btn-outline">Upload</button>
+        <label
+          :class="{ loading: uploading }"
+          :disabled="uploading"
+          class="btn btn-primary btn-outline"
+        >
+          <input type="file" @change="onChange" class="hidden" />
+          Upload
+        </label>
         <button class="btn btn-primary ml-3">Publish</button>
       </div>
       <div class="mt-8">
-        <div class="gallery ">
-          <div>
-            <img src="https://via.placeholder.com/300" alt="" />
+        <div v-if="images.length" class="gallery">
+          <div v-for="img in images" :key="img.id">
+            <img :src="img.thumbnailLink" referrerPolicy="no-referrer" />
           </div>
-          <div>
-            <img src="https://via.placeholder.com/300" alt="" />
-          </div>
-          <div>
-            <img src="https://via.placeholder.com/300" alt="" />
-          </div>
-          <div>
-            <img src="https://via.placeholder.com/300" alt="" />
-          </div>
-          <div>
-            <img src="https://via.placeholder.com/300" alt="" />
-          </div>
-          <div>
-            <img src="https://via.placeholder.com/300" alt="" />
-          </div>
+        </div>
+        <div v-else class="text-center mt-20">
+          <span
+            v-if="loading"
+            class="loading btn bg-transparent text-black border-none"
+            >Loading...</span
+          >
+          <span v-else>No images</span>
         </div>
       </div>
     </div>
@@ -33,7 +33,53 @@
 
 <script>
 export default {
-  middleware: "auth"
+  middleware: "auth",
+  data() {
+    return {
+      file: null,
+      images: [],
+      uploading: false,
+      loading: false
+    };
+  },
+  methods: {
+    onChange(e) {
+      this.file = e.target.files[0];
+      e.target.value = "";
+      this.upload();
+    },
+    async upload() {
+      this.uploading = true;
+      try {
+        const formData = new FormData();
+        formData.append("image", this.file);
+
+        const res = await this.$axios.put("/upload/drive", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data"
+          }
+        });
+
+        this.images.unshift(res.data);
+      } catch (error) {
+        console.error(error);
+      }
+      this.uploading = false;
+    },
+    async getImages() {
+      this.loading = true;
+      try {
+        const res = await this.$axios.get(`/gallery/1/images`);
+        this.images = res.data;
+      } catch (error) {
+        console.error(error);
+      }
+      this.loading = false;
+    }
+  },
+  mounted() {
+    this.getImages();
+  }
 };
 </script>
 
