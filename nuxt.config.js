16ba5841b104b30f573171d3269eb7752f1a3a37
ezl <ericzliu@gmail.com>
2023-01-29 17:10:09 -0600

    update favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,6 +23,11 @@ export default {
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "true" },
       { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Lora:wght@500&family=Poppins:ital,wght@0,100;0,300;0,600;1,100;1,300&display=swap" },
+
+      { rel:"apple-touch-icon", sizes:"180x180", href:"/apple-touch-icon.png" },
+      { rel:"icon", type:"image/png", sizes:"32x32", href:"/favicon-32x32.png" },
+      { rel:"icon", type:"image/png", sizes:"16x16", href:"/favicon-16x16.png" },
+      { rel:"manifest", href:"/site.webmanifest" },
     ]
   },
 
