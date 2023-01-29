6206fedf1181b764d490784bc6d8b66f7023a65e
ezl <ericzliu@gmail.com>
2023-01-28 21:31:21 -0600

    add overlay

--- components/GalleryItem.vue
+++ components/GalleryItem.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="item" @click="itemClicked">
-    <img :src="item.thumbnailLink" referrerPolicy="no-referrer" />
+    <div class="imageContainer">
+      <img :src="item.thumbnailLink" referrerPolicy="no-referrer" />
+      <div class="overlay">
+        {{ item.name }}
+      </div>
+    </div>
     <div class='itemDescription'>
       <h5 class='itemTitle'>{{item.name}}</h5>
       <p v-if="item.content"> {{item.content}}</p>
@@ -30,19 +35,37 @@ export default {
 .item {
   cursor: pointer;
 }
-.item:hover {
-  filter: grayscale(50%) blur(3px);
+.imageContainer {
+  position: relative;
 }
-
-.item > img {
-  @apply rounded-2xl;
+.imageContainer img, .imageContainer .overlay {
+  @apply rounded-lg;
+}
+.imageContainer img {
+  display: block;
   width: 100%;
-  object-fit: cover;
 }
 .itemTitle {
   @apply text-base text-white mb-1;
  }
 .itemDescription {
   @apply text-sm text-slate-300 mt-2 font-light;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  background-color: rgba(0, 0, 0, 0.4);
+  padding: 1em;
+}
+
+.imageContainer:hover .overlay {
+  opacity: 1;
 }
 </style>
